951389748988aa59d9f1e0d66e0c24b992bc4cb2
Chely Ho <adept_inept@yahoo.com>
2017-11-06 04:44:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+//const customerFeet = 150;
+//const customerCity = 'Houston';
+//const customerTip = 'generous';
+
+function scuberGreetingForFeet(customerFeet){
+  if (customerFeet<=400){
+    return 'This one is on me!';
+  } else if (customerFeet>2000 && customerFeet<2500){
+    return  'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(customerCity){
+  return customerCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(customerTip){
+  switch (customerTip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+  default:
+    return 'Bye.'   //Cheapskate!
+    break;
+}
+}
